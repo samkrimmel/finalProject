546fd35b08e7523ce5bffa70557d8a68f691a707
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-24 08:06:06 -0400

    Updated from Brython Server: 5/24/2018 8:06:02 AM

--- gameOfLife.py
+++ gameOfLife.py
@@ -4,17 +4,26 @@
 
 #CONSTANTS
 
+boardHeight = 12
+boardWidth = 12
+
 #FUNCTIONS
 
 buildBoard():
-    
+    board = [0]*boardHeight
+    for i in range(0,12):
+        board[i].append(0)
+    print(board)
+
 redrawAll():
     
 numNeighbors(row,column):
 
 nextGeneration():
+    
+mouseClick(event):
 
-
+buildBoard()
 
 
 
